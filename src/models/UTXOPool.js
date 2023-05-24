1d6ea8ac23f227c68dd89fc1209cf4233ea87bad
unknown <2029054066@qq.com>
2023-05-24 21:01:29 +0800

    myfirst commit

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,21 +1,45 @@
 import UTXO from './UTXO.js'
+import Transaction from './Transaction.js';
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+    constructor(utxos = {}) {
+        this.utxos = utxos
+    }
 
-  addUTXO(publicKey, amount) {}
+    // 添加交易函数
+    /**
+     * 将交易的信息更新至 UTXOPool 中
+     */
+    addUTXO(utxo) {
+        // 将新的交易添加进UTXO池中并更新余额
 
-  clone() {}
+        if (this.utxos[utxo.publicKey] != null) {
+            this.utxos[utxo.publicKey] = { amount: this.utxos[utxo.publicKey].amount + utxo.amount };
+        } else {
+            this.utxos[utxo.publicKey] = { amount: utxo.amount };
+        }
+    }
 
-  // 处理交易函数
-  handleTransaction() {}
+    // 将当前 UXTO 的副本克隆
+    clone() {
+            return this.utxos
+        }
+        // 验证hash的算法
+        // 返回一个bool值
+    isValidTransaction(miner, num) {
+        console.log(this.utxos)
+        return this.utxos[miner].amount > num
 
-  // 验证交易合法性
-  /**
-   * 验证余额
-   * 返回 bool 
-   */
-  isValidTransaction() {}
+    }
+
+    // 处理建议的方法
+    handleTransaction(tra) {
+        // 首先构建一个UTXO
+        if (this.isValidTransaction(tra.miner, tra.num)) {
+            this.addUTXO(new UTXO(tra.receiverPubKey, tra.num))
+            this.utxos[tra.miner] = { amount: this.utxos[tra.miner].amount - tra.num }
+        }
+    }
 }
 
 export default UTXOPool
